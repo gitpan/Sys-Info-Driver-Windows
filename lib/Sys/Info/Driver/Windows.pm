e11dee185f7d285ec57825a4071d60fcec6d0325
Burak Gürsoy <burak@cpan.org>
2009-03-05 02:11:08 +0000

    Import of BURAK/Sys-Info-Driver-Windows-0.69_02 from CPAN.
    gitpan-cpan-distribution: Sys-Info-Driver-Windows
    gitpan-cpan-version:      0.69_02
    gitpan-cpan-path:         BURAK/Sys-Info-Driver-Windows-0.69_02.tar.gz
    gitpan-cpan-author:       BURAK
    gitpan-cpan-maturity:     developer

--- lib/Sys/Info/Driver/Windows.pm
+++ lib/Sys/Info/Driver/Windows.pm
@@ -14,7 +14,7 @@ use constant SM_SERVERR2    => 89; # Windows Server 2003 R2
 use Exporter ();
 use XSLoader;
 
-$VERSION = '0.69_01';
+$VERSION = '0.69_02';
 @ISA     = qw( Exporter );
 @EXPORT  = qw(  );
 %EXPORT_TAGS = (
